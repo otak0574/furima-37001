bad6ad55c5782f99ce6d2d87cd2a97d996054928
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-01 10:54:41 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :nickname, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 end
