518e986b83cfc7a698be8ce5733a0b7f789e8569
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-02 08:14:29 +0900

    商品の状態追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   end
 
   def new
+    
   end
 
   def create
--- app/models/item_info.rb
+++ app/models/item_info.rb
@@ -2,7 +2,10 @@ class ItemInfo < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   # belongs_to :prefecture
   belongs_to :category
-
+  belongs_to :spec
+  belongs_to :shipping_fee
+  belongs_to :pref
+  belongs_to :shipping_day
 
 
   validates :title, :text, presence: true
