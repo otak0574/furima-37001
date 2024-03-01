c5090f4e3d23b7a45f0cd966d59ec6aab6472fa7
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-01 19:32:51 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "items#index"
-  resources :items ,only: :index
+  resources :items ,only: [:index, :new, :edit, :update, :edit]
   # Defines the root path route ("/")
   # root "articles#index"
 end
