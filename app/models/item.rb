dccaa6ecaaeeb38ce2f65e6b53597b6848add193
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-01 17:18:49 +0900

    バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true, format: {with: /\A[0-9]+\z/} do
+    validates :price
+  end
 end
