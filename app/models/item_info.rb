5ba30d309f2fee260d797c1d409d504b700d6d2b
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-02 09:17:29 +0900

    発送元の地域の追加

--- app/models/item_info.rb
+++ app/models/item_info.rb
@@ -7,7 +7,6 @@ class ItemInfo < ApplicationRecord
   belongs_to :pref
   belongs_to :shipping_day
 
-
   validates :title, :text, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
