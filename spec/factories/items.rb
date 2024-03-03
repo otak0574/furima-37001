dd1ec6c47b1ff2ad485b32cefa4cb4604fadac96
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-03 10:26:14 +0900

    単体テストコードの実装

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    name              { '商品の名前' }
+    description       {'商品の説明'}
+    price             {10000}
+    category_id       {3}
+    spec_id           {3}
+    pref_id           {5}
+    shipping_fee_id   {2}
+    shipping_day_id   {2}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/de.png'), filename: 'de.png', content_type: 'image/png')
+    end
+  end
+end
