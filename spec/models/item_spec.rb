86b73996b4bbaa3a0c213337ad20320465ae46f2
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-03 15:06:09 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
-      it '価格は、~¥9,999,999以下で保存可能であること' do
+      it '価格は、¥9,999,999以下で保存可能であること' do
         @item.price = '1000000000000000'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
