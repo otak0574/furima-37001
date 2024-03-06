85c19fa26b58796b2ce03aa5b16abb656b5a8f05
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-06 09:54:51 +0900

    モデルとマイグレーションの編集

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,16 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :post_number, :pref_id, :city, :street_address, :building, :telephone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(- " }
+    validates :pref_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
+
+  def save
+    purchase_history = PurchaseHistory.create(user_id: user_id, item_id: item_id )
+    ShippingAddress.create(purchase_history_id: purchase_history.id, post_number: post_number, city: city, street_address: street_address, building: building, telephone_number: telephone_number, pref_id: pref_id)
+  end
+end
