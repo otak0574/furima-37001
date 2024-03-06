a93dd6af79586c0d8a064f05c99778a347d7e616
otak0574 <159395656+otak0574@users.noreply.github.com>
2024-03-06 22:16:33 +0900

    カード機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_05_124110) do
     t.string "first_name", default: "", null: false
     t.string "last_name_kana", default: "", null: false
     t.string "first_name_kana", default: "", null: false
-    t.date "birthday"
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
